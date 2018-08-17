e4c4d0a40138c812eaa535f7e5e9bf78d7b5c672
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-17 22:49:21 +0530

    create-react-app Setup

---
+++ react-redux-101/src/App.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" style={{textAlign : 'center'}}>
+        <header className="App-header">
+          <h1 className="App-title">Welcome to My React App</h1>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
