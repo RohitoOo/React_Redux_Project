4ffd9f3679848975b8cb51851eeec0c5079d73d0
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-17 23:48:54 +0530

    Outputting Data Via Props And Deconstructing Props

--- react-redux-101/src/App.js
+++ react-redux-101/src/App.js
@@ -1,14 +1,23 @@
 
 import React, { Component } from 'react';
-
+import FootBallers from './components/footballers'
 
 class App extends Component {
+
+  state = {
+    soccerPlayers : [
+      {id: 1, name: "Christiano Ronaldo" , position: "Right Forward" , club: "Juventus"},
+      {id: 2,name: "Rooney" , position: "Forward" , club: "Manchester United"},
+      {id: 3,name: "Sergio Ramos" , position: "Center Back" , club: "Real Madrid"},
+    ]
+  }
   render() {
     return (
       <div className="App" style={{textAlign : 'center'}}>
         <header className="App-header">
           <h1 className="App-title">Welcome to My React App</h1>
         </header>
+        <FootBallers soccerPlayers={this.state.soccerPlayers} />
       </div>
     );
   }
---
+++ react-redux-101/src/components/footballers.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+
+class FootBallers extends Component {
+
+  render() {
+    return (
+      <div>
+
+        <table>
+          <tbody>
+          <tr>
+            <th>Soccer Player</th>
+            <th>Position</th>
+            <th>Club</th>
+          </tr>
+         {this.props.soccerPlayers.map( (player) => {
+          return  <tr key={player.id}>
+                    <td >{player.name}</td>
+                    <td >{player.position}</td>
+                    <td >{player.club}</td>
+                    </tr>
+        })}
+        </tbody>
+      </table>
+      </div>
+    );
+  }
+
+}
+
+export default FootBallers;
