7c12ac37213b6f4e66324d26957d05200d6e1e1b
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-18 01:26:25 +0530

    Props Deconstruction In Functional Components

--- react-redux-101/src/components/footballers.js
+++ react-redux-101/src/components/footballers.js
@@ -1,42 +1,6 @@
-// import React, { Component } from 'react';
-//
-//
-//
-// class FootBallers extends Component {
-//
-//   render() {
-//     return (
-//       <div>
-//
-//         <table>
-//           <tbody>
-//           <tr>
-//             <th>Soccer Player</th>
-//             <th>Position</th>
-//             <th>Club</th>
-//           </tr>
-//          {this.props.soccerPlayers.map( (player) => {
-//           return  <tr key={player.id}>
-//                     <td >{player.name}</td>
-//                     <td >{player.position}</td>
-//                     <td >{player.club}</td>
-//                     </tr>
-//         })}
-//         </tbody>
-//       </table>
-//       </div>
-//     );
-//   }
-//
-// }
-//
-// export default FootBallers;
-
-
-
 import React from 'react';
 
-const FootBallers = (props) => {
+const FootBallers = ({soccerPlayers}) => {
   return (
   <div>
     <table>
@@ -46,7 +10,7 @@ const FootBallers = (props) => {
         <th>Position</th>
         <th>Club</th>
       </tr>
-     {props.soccerPlayers.map( (player) => {
+     {soccerPlayers.map( (player) => {
       return  <tr key={player.id}>
                 <td >{player.name}</td>
                 <td >{player.position}</td>
