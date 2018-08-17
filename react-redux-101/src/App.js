b613a7f3dc798f8c1691f5bf132f757d8be7c33e
Rohit Bhambhani <rohit.m.bhambhani@gmail.com>
2018-08-18 01:22:21 +0530

    Container ( Class ) Components VS Functional Components

--- react-redux-101/src/App.js
+++ react-redux-101/src/App.js
@@ -24,3 +24,18 @@ class App extends Component {
 }
 
 export default App;
+
+
+// Container Component (Class Component) VS UI Component
+
+// Container Component
+// 1. Contain State
+// 2. Contain Life cycle hooks
+// 3. Not Concerned with UI
+// 4. Use Classe to create these components 
+//
+// UI Components
+// 1. Does not contain State ( rec data from Container Comp)
+// 2. Receive Data from props
+// 3. Only concerned with UI
+// 4. Use Functions instead of classses to create these compoenets
--- react-redux-101/src/components/footballers.js
+++ react-redux-101/src/components/footballers.js
@@ -1,33 +1,61 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+//
+//
+//
+// class FootBallers extends Component {
+//
+//   render() {
+//     return (
+//       <div>
+//
+//         <table>
+//           <tbody>
+//           <tr>
+//             <th>Soccer Player</th>
+//             <th>Position</th>
+//             <th>Club</th>
+//           </tr>
+//          {this.props.soccerPlayers.map( (player) => {
+//           return  <tr key={player.id}>
+//                     <td >{player.name}</td>
+//                     <td >{player.position}</td>
+//                     <td >{player.club}</td>
+//                     </tr>
+//         })}
+//         </tbody>
+//       </table>
+//       </div>
+//     );
+//   }
+//
+// }
+//
+// export default FootBallers;
 
 
 
-class FootBallers extends Component {
+import React from 'react';
 
-  render() {
-    return (
-      <div>
-
-        <table>
-          <tbody>
-          <tr>
-            <th>Soccer Player</th>
-            <th>Position</th>
-            <th>Club</th>
-          </tr>
-         {this.props.soccerPlayers.map( (player) => {
-          return  <tr key={player.id}>
-                    <td >{player.name}</td>
-                    <td >{player.position}</td>
-                    <td >{player.club}</td>
-                    </tr>
-        })}
-        </tbody>
-      </table>
-      </div>
-    );
-  }
-
-}
+const FootBallers = (props) => {
+  return (
+  <div>
+    <table>
+      <tbody>
+      <tr>
+        <th>Soccer Player</th>
+        <th>Position</th>
+        <th>Club</th>
+      </tr>
+     {props.soccerPlayers.map( (player) => {
+      return  <tr key={player.id}>
+                <td >{player.name}</td>
+                <td >{player.position}</td>
+                <td >{player.club}</td>
+                </tr>
+    })}
+    </tbody>
+  </table>
+  </div>
+)}
 
 export default FootBallers;
